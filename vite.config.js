54738e57d3b29b77542f70b45de27ddee5f0011f
RozzReaper <leo1000player@gmail.com>
2024-06-25 22:51:42 -0600

    Refactor Vite config to use absolute file paths #2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';  // Añadimos esta línea
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        about: './about.html',
-        blog: './blog.html',
-        contactenos: './contactenos.html',
-        donaciones: './donaciones.html',
-        eventos: './eventos.html',
-        galeria: './galeria.html',
-        programas: './programas.html',
-        faq: './faq.html',
-        articulo1: './articulo1.html',
-        articulo2: './articulo2.html'
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contactenos: resolve(__dirname, 'contactenos.html'),
+        donaciones: resolve(__dirname, 'donaciones.html'),
+        eventos: resolve(__dirname, 'eventos.html'),
+        galeria: resolve(__dirname, 'galeria.html'),
+        programas: resolve(__dirname, 'programas.html'),
+        faq: resolve(__dirname, 'faq.html'),
+        articulo1: resolve(__dirname, 'articulo1.html'),
+        articulo2: resolve(__dirname, 'articulo2.html')
       }
     }
   },
