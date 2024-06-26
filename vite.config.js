4fa30a839e04b9a0c632b3829b093a806f89b593
RozzReaper <leo1000player@gmail.com>
2024-06-25 22:49:54 -0600

    Refactor Vite config to consolidate input file paths

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,27 @@
 import { defineConfig } from 'vite';
-import path, { resolve } from 'node:path';
-import fs from 'node:fs';
-import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import htmlPurge from 'vite-plugin-purgecss';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  base: "/Galeria-Metropolitana-de-Arte/",
-  appType: 'mpa',
   build: {
     rollupOptions: {
-      input: Object.fromEntries(
-        fs.readdirSync('./handles').filter((file) => file.endsWith('.hbs')).map((file) => [
-          file.slice(0, file.length - path.extname(file).length),
-          resolve(process.cwd(), 'handles', file),
-        ])
-      ),
-    },
+      input: {
+        main: './index.html',
+        about: './about.html',
+        blog: './blog.html',
+        contactenos: './contactenos.html',
+        donaciones: './donaciones.html',
+        eventos: './eventos.html',
+        galeria: './galeria.html',
+        programas: './programas.html',
+        faq: './faq.html',
+        articulo1: './articulo1.html',
+        articulo2: './articulo2.html'
+      }
+    }
   },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
-      // Añadir cualquier otra configuración que necesites para el plugin de Handlebars
-    }),
-    htmlPurge({}),
-    ViteMinifyPlugin({}),
-  ],
-  assetsInclude: ['**/*.hbs'], // Añadir esta línea para incluir archivos .hbs como activos
+    })
+  ]
 });
